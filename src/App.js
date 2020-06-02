67c17ae1100690cb5b13d1eb94531a21f755bcee
unknown <gron1gh1@ajou.ac.kr>
2020-06-02 20:07:36 +0900

    (Fix) TreeView

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useMemo } from 'react';
+import React, { useState, useContext, useMemo, useEffect } from 'react';
 import { transform } from 'buble';
 import axios from 'axios';
 import 'antd/dist/antd.css';
@@ -11,13 +11,53 @@ import { IFrame } from './IFrame';
 import { CodeEditor } from './CodeEditor';
 const { TreeNode } = Tree;
 
+function getNpmData(lib)// ImportNPM to Tree Data
+{
+    let main = document.getElementById('frame').contentWindow;
+    let npm_infos = ImportNPM(lib);
+
+    let treeData = npm_infos.map((v) => {
+  
+      return {
+        title: v.name,
+        key: v.name,
+        children: Object.keys(v.module).map(m => {
+          return {
+            title: m,
+            key: v.name + '-' + m,
+          }
+        })
+      }
+    });
+  //  IframeData.SetData(treeData, 'PUSH');
+    console.log('treeData', treeData);
+    return treeData;
+}
+
+function ImportNPM(lib_names) { // Import NPM Module imported within <iframe>
+  let all = Object.keys(document.getElementById('frame').contentWindow);
+  return all.filter((v) => lib_names.some((a) => {
+
+    return a.replace(/-/g,"") === v;
+  })).map((v) => {
+    return {
+      name: v,
+      module: document.getElementById('frame').contentWindow[v]
+    }
+  });
+}
+
 const TreeView = () => {
   const [expandedKeys, setExpandedKeys] = useState([]);
   const [checkedKeys, setCheckedKeys] = useState([]);
   const [selectedKeys, setSelectedKeys] = useState([]);
   const [autoExpandParent, setAutoExpandParent] = useState(true);
   const get_data = useContext(IFrameContext).data;
-
+  const [data,setData] = useState([{
+    title: 'NPM Module',
+    key: 'NPM Module',
+    children: [],
+}]);
   const onExpand = expandedKeys => {
     console.log('onExpand', expandedKeys); // if not set autoExpandParent to false, if children expanded, parent can not collapse.
     // or, you can remove all expanded children keys.
@@ -33,9 +73,20 @@ const TreeView = () => {
 
   const onSelect = (selectedKeys, info) => {
     console.log('onSelect', info);
+    if(info.node.title === "NPM Module")
+    {
+      
+      setData([{
+        title: 'NPM Module',
+        key: 'NPM Module',
+        children: getNpmData(['antd', 'reactstrap','react-custom-scroll']),
+    }]);
+    }
     setSelectedKeys(selectedKeys);
   };
-
+useEffect(()=>{
+console.log('dt',data);
+},[data])
   return (
     <Tree
       checkable
@@ -46,7 +97,7 @@ const TreeView = () => {
       checkedKeys={checkedKeys}
       onSelect={onSelect}
       selectedKeys={selectedKeys}
-      treeData={get_data}
+      treeData={data}
     />
   );
 };
@@ -66,7 +117,6 @@ function App() {
   }
   `
 
-  return useMemo(() => {
     return (
       <IFrameProvider>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
@@ -87,8 +137,7 @@ function App() {
           </div>
         </div>
       </IFrameProvider>
-    );
-  }, []);
+    )
 }
 
 export default App;
